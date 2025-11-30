6457f018851449d5b81b210be8b6024c26770139
JotaroKujoo <rdrgz.jose123@gmail.com>
2025-12-01 00:52:01 +0100

    add: Giving all the comments to every important section on code and modifying readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OpenAI-chat-script
 Testing with scripts in Python for OpenAI API
+
+emojis from project by https://getemoji.com/
--- chatWithUser.py
+++ chatWithUser.py
@@ -1,23 +1,32 @@
 # IMPORTING LIQBRARIES
 
+#Importing the requests library to make HTTP/S requests
 import requests
+
+#Importing the os library to interact with the operating system
 import os
+
+# Importing the load_dotenv function from the dotenv library to load environment variables from a .env file
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
 
+# Retrieving the OpenAI API key from environment variables
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Chat loop
 while True:
     user = input("You: ")
 
+    # Exit condition
     if user.lower() in ["exit", "quit", "salir"]:
         print("Exiting chat. Goodbye!")
         break
-
+    
+    # Error handling
     try:
+        
         # Making request to AIML API
         response = requests.post(
             "https://api.aimlapi.com/v1/responses",
@@ -34,8 +43,8 @@ while True:
         # Raises an error if status code is not 200
         response.raise_for_status()
 
+        #Printing the AI response
         data = response.json()
-
         print("AI:", data["output_text"])
 
     except KeyError as e:
